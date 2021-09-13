b5f1ca61220357ee289abf72754cdc98e30c1f4e
Michele Valente <valentemichele89@gmail.com>
2021-09-13 19:08:53 +0200

    creato messaggio redirect

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
     <h1>Mi vedi?????</h1>
+
+
+    @if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+    @endif
 </x-layout>
