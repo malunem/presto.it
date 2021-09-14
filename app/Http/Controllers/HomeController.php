8bfa6977ad860f032cdd84b7700b6a4ada91cd9b
Danilo Dinoi <danil96@hotmail.com>
2021-09-14 11:19:28 +0200

    aggiunta card homepage con dati inseriti

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('homepage');
+
+        $announcements=Announcement::orderby('created_at', 'desc')->take(5)->get();
+        return view('homepage', compact('announcements'));
+        
     }
+    
 }
+
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
-    <h1>Mi vedi?????</h1>
+    <h1>Annunci pubblicati</h1>
+
 
 
     @if (session('message'))
@@ -7,4 +8,25 @@
         {{ session('message') }}
     </div>
     @endif
+    <div class="container">
+        <div class="row">
+            @foreach ($announcements as $announcement)
+            <div class="col-12 col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://picsum.photos/300" class="card-img-top" alt="{{$announcement->title}}">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$announcement->title}}</h5>
+                      <p class="card-text">{{$announcement->description}}</p>
+                      <p class="card-text">{{$announcement->user->name}}</p>
+                      <p class="card-text">{{$announcement->created_at->format('d/m/Y')}}</p>
+                      <p class="card-text"> <a href="#">{{$announcement->category->category}}</a></p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+                
+            @endforeach
+           
+        </div>
+    </div>
 </x-layout>
