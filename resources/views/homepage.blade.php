65c5ffa572662daf75f005e76b7030d2335b1ef6
Michele Valente <valentemichele89@gmail.com>
2021-09-15 10:03:10 +0200

    inseriti annunci

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,10 @@
         {{ session('message') }}
     </div>
     @endif
-    <div class="carousel ">
+    <header>
+      <img class="w-100 d-block header" src="https://www.apexweb.co.uk/docs/headers/e-commerce-websites.jpg" alt="">
+    </header>
+    {{-- <div class="carousel ">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -17,7 +20,7 @@
                 <div class="carousel-caption text-red img-carousel">
                   <h2>I tuoi acquisti a portata di click</h2>
                   <h3>Veloce, reattivo e affidabile .</h3>
-                  <img src="http://picsum.photos/300"  alt="">
+                  
                 </div>
               </div>
               <div class="carousel-item sfondo2">
@@ -43,18 +46,18 @@
             </button>
           </div>
 
-    </div>
+    </div> --}}
     <div class="wrapper">
         <div class="container">
             <div class="row">
-                <h1 class="mt-3 text-center">Annunci pubblicati</h1>
+                <h1 class=" my-4 py-5 text-center">Annunci pubblicati</h1>
                 @foreach ($announcements as $announcement)
                 <div class="col-12 col-md-4">
                     <div class="card my-3" style="width: 18rem;">
                         <img src="https://picsum.photos/300" class="card-img-top" alt="{{$announcement->title}}">
                         <div class="card-body">
                           <h5 class="card-title">{{$announcement->title}}</h5>
-                          <p class="card-text">{{$announcement->description}}</p>
+                          <p class="card-text">{{Str::limit($announcement->description, 50)}}</p>
                           <p class="card-text">{{$announcement->user->name}}</p>
                           <p class="card-text">{{$announcement->created_at->format('d/m/Y')}}</p>
                           <p class="card-text"> <a href="{{route('show.Category', $announcement->category_id)}}" class="text-decoration-none ">{{$announcement->category->category}}</a></p>
