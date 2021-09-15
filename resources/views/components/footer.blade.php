e08ecf6846daa1df8e15d3dd0cc8459aee6e45b5
Michele Valente <valentemichele89@gmail.com>
2021-09-15 10:15:00 +0200

    sistemato footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,10 @@
             <div class="col-12 col-md-4 me-5 ">
                 <h3 class="my-2 "><img class="bg-primary" alt="">Presto.it<i class="fab fa-google-play mx-3"></i>
                     <i class="fab fa-apple fa-1x"></i></h2> 
-                    <p class="mt-5 mb-5">Con curiosità e impegno inesauribili, ci dedichiamo da anni all'esplorazione del mondo del cinema e delle serie TV: spazio all'informazione, alle recensioni, all'approfondimento e all'analisi, ma anche e soprattutto al divertimento e alla passione.</p>
+                    <p class="mt-5 mb-5">Devi fare spazio in casa? O cerchi di fare un affare nel mercato dell'usato? Presto è la piattaforma di annunci che combina un vasto assortimento di articoli a un ottimo servizio di supporto.
+                        È il punto di riferimento per i siti di annunci gratuiti in Italia, il sito giusto per i tuoi acquisti online.
+                        Non solo! Cerchi subito un lavoro o una nuova casa? Le inserzioni pubblicate includono offerte di impiego, case in affitto o in vendita e tanto altro.
+                        Cosa aspetti? Comincia subito a comprare online su presto.it!</p>
                     
                     <i class="fab fa-facebook-square fa-2x ms-3"></i>
                     <i class="fab fa-instagram fa-2x  ms-4"></i>
@@ -19,11 +22,11 @@
                     </div>
                 </div>
                 <div class="col-12 col-md-6 mt-5">
-                    <p>DUESSE COMMUNICATION S.r.l. Via G.Richard 1 – Torre A - 20143 Milano - PI 12777410155 - N. REA 1584599 - N. Reg. Imp. MI-1999/96457 Capitale sociale € 203.000 - Iscrizione al Registro Nazionale della Stampa n.9380 del 11/4/2001 ROC n.6794 Bestmovie.it è una testata giornalistica registrata al Tribunale di Milano - Registro Stampa - n. 12 del 12/01/2002 Direttore Responsabile Vito Sinopoli Copyright ©️ 2004-2018 DUESSE COMMUNICATION S.r.l. - Tutti i diritti riservati - Privacy Policy - Credits: BigUp.Marketing</p>
+                    <p>© 2021 Presto.it - P.IVA 05526340962</p>
                     
-                    <p class="mt-5">©️ 2021 NetAddiction Srl P.iva: 01206540559 – Sede Legale: Via A.M. Angelini, 12 - 05100 Terni - tel:07442462 Capitale sociale: Euro 119.000 – Iscrizione al Registro delle Imprese di Terni n.01206540559</p>
+                    <p class="mt-5">©️ 2021 presto Srl P.iva: 01206540559 – Sede Legale: Via A.M. Angelini, 12 - 05100 Terni - tel:07442462 Capitale sociale: Euro 119.000 – Iscrizione al Registro delle Imprese di Terni n.01206540559</p>
                     
-                    <p class="mt-5">©️2001 - 2021 Anicaflash S.r.l. - P.Iva 01816001000 Testata Giornalistica registrata al Tribunale di Roma n.332 del 11.07.2001</p>
+                    <p class="mt-5">©️2001 - 2021 presto S.r.l. - P.Iva 01816001000 Testata registrata al Tribunale di Roma n.332 del 11.07.2001</p>
                     
                 </div>
                 
