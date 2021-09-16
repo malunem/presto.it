a3f02ee8bf2741253c80896b379dc9ab2b6297c7
Danilo Dinoi <danil96@hotmail.com>
2021-09-16 13:07:50 +0200

    aggiunto logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbarcustom navbar-expand-lg navbar-dark ">
     <div class="container-fluid">
    
-      <a  class="navbar-brand" href=""><img class="img-fluid logo" src="/img/logo.png" alt="">Presto.it</a>
+      <a  class="navbar-brand" href=""><img class="img-fluid logo" src="/img/sfondopresto2.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
