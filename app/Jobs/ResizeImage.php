bb2f3e8937b5cbdf7aeb14b649658ba0ae2268b2
Michele Valente <valentemichele89@gmail.com>
2021-09-22 16:43:31 +0200

    sistemato watermark

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Spatie\Image\Image;
 use Illuminate\Bus\Queueable;
 use Spatie\Image\Manipulations;
+use App\Models\AnnouncementImage;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -32,6 +33,7 @@ class ResizeImage implements ShouldQueue
     //  */
     public function handle()
     {
+        
         $w = $this->w;
         $h = $this->h;
         
@@ -39,8 +41,32 @@ class ResizeImage implements ShouldQueue
         
 
         $destPath = storage_path() . '/app/' . $this->path . "/crop{$w}x{$h}_" . $this->fileName;
-        Image::load($srcPath)
+        // Image::load($srcPath)
+        // ->crop(Manipulations::CROP_CENTER, $w, $h);
+        
+        // $i = AnnouncementImage::find($this->announcement_image_id);
+        // if(!$i){
+        //     return;
+        // }
+        // $srcPath=storage_path('/app/' .$i->file);
+        
+        $image = Image::load($srcPath)
         ->crop(Manipulations::CROP_CENTER, $w, $h)
+        ->watermark(base_path('public/img/sfondoprestoPiccolo.png'))
+        ->watermarkPosition('bottom-right')
+        // ->watermarkPosition(Manipulations::POSITION_CENTER)
+        // ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
+        // ->watermarkWidth(10, Manipulations::UNIT_PERCENT)
+        // ->watermarkPadding(50)
+        // ->watermarkOpacity(45);
+        ->watermarkPadding(20)
+        // ->watermarkPosition(Manipulations::POSITION_CENTER)
+        ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
+        ->watermarkWidth(20, Manipulations::UNIT_PERCENT)
+        // ->watermarkFit(Manipulations::FIT_STRETCH)
+        ->watermarkOpacity(45)
+
+       
         ->save($destPath);
       
     }
--- app/Jobs/Watermark.php
+++ app/Jobs/Watermark.php
@@ -30,27 +30,27 @@ class Watermark implements ShouldQueue
      */
     public function handle()
     {
-        $i = AnnouncementImage::find($this->announcement_image_id);
-        if(!$i){
-            return;
-        }
-        $srcPath=storage_path('/app/' .$i->file);
+        // $i = AnnouncementImage::find($this->announcement_image_id);
+        // if(!$i){
+        //     return;
+        // }
+        // $srcPath=storage_path('/app/' .$i->file);
         
-        $image = Image::load($srcPath);
-        $image->watermark(base_path('public/img/sfondopresto1.png'))
-        ->watermarkPosition('bottom-right')
-        // ->watermarkPosition(Manipulations::POSITION_CENTER)
+        // $image = Image::load($srcPath);
+        // $image->watermark(base_path('public/img/sfondoprestoPiccolo.png'))
+        // ->watermarkPosition('bottom-right')
+        // // ->watermarkPosition(Manipulations::POSITION_CENTER)
+        // // ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
+        // // ->watermarkWidth(10, Manipulations::UNIT_PERCENT)
+        // // ->watermarkPadding(50)
+        // // ->watermarkOpacity(45);
+        // ->watermarkPadding(20, 3, Manipulations::UNIT_PERCENT)
+        // // ->watermarkPosition(Manipulations::POSITION_CENTER)
         // ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
-        // ->watermarkWidth(10, Manipulations::UNIT_PERCENT)
-        // ->watermarkPadding(50)
+        // // ->watermarkWidth(50, Manipulations::UNIT_PERCENT)
+        // // ->watermarkFit(Manipulations::FIT_STRETCH)
         // ->watermarkOpacity(45);
-        ->watermarkPadding(20, 3, Manipulations::UNIT_PERCENT)
-        // ->watermarkPosition(Manipulations::POSITION_CENTER)
-        ->watermarkHeight(20, Manipulations::UNIT_PERCENT)
-        // ->watermarkWidth(50, Manipulations::UNIT_PERCENT)
-        // ->watermarkFit(Manipulations::FIT_STRETCH)
-        ->watermarkOpacity(45);
 
-        $image->save($srcPath);
+        // $image->save($srcPath);
     }
 }
